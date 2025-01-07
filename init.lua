051f7a6b01f614028dd1fc254562a0d079b66421
Federico Di Filitto <federico.difilitto@protonmail.com>
2025-01-07 10:31:16 +0100

    feat(lsp): added volar to mason packages

--- init.lua
+++ init.lua
@@ -627,6 +627,7 @@ require('lazy').setup({
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         --
+        volar = {},
 
         ts_ls = {
           filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
